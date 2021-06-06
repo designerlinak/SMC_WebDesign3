6979aa17166b3d87a803a0a3c976bdfb5a92fa5e
designerlinak <66003403+designerlinak@users.noreply.github.com>
2021-06-07 01:53:51 +0800

    Update on final

---
+++ week15:final/js/checkout-form-validation.js
@@ -0,0 +1,88 @@
+/*TOGGLE BILLING VISIBILITY*/
+function toggleBilling() {
+    if (document.getElementById('customSwitches').checked == true) {
+        document.getElementById('billingGroup').style.display = "none";
+        
+    } else {
+        document.getElementById('billingGroup').style.display = "block";
+      
+    }
+}
+
+/*CHECK VALID INPUT*/
+var errorInput = document.getElementsByClassName('invalid-input');
+
+/*message if user inputs numbers instead of letters*/
+function alphaOnly() {
+    var fname = document.getElementById('txtfname');
+    var lname = document.getElementById('txtlname');
+    var state = document.getElementById('txtstate');
+    var city = document.getElementById('txtcity');
+    var Bfname = document.getElementById('txtfname_billing');
+    var Blname = document.getElementById('txtlname_billing');
+    var Bstate = document.getElementById('txtstate_billing');
+    var Bcity = document.getElementById('txtcity_billing');
+
+    var numbers = "^[0-9*#+@]+$";
+    if (fname.value.match(numbers) && document.activeElement == fname) {
+        errorInput[0].style.display = "block";
+    } else if (lname.value.match(numbers) && document.activeElement == lname) {
+        errorInput[1].style.display = "block";
+    } else if (state.value.match(numbers) && document.activeElement == state) {
+        errorInput[2].style.display = "block";
+    } else if (city.value.match(numbers) && document.activeElement == city) {
+        errorInput[3].style.display = "block";
+    } else if (Bfname.value.match(numbers) && document.activeElement == Bfname) {
+        errorInput[5].style.display = "block";
+    } else if (Blname.value.match(numbers) && document.activeElement == Blname) {
+        errorInput[6].style.display = "block";
+    } else if (Bstate.value.match(numbers) && document.activeElement == Bstate) {
+        errorInput[7].style.display = "block";
+    } else if (Bcity.value.match(numbers) && document.activeElement == Bcity) {
+        errorInput[8].style.display = "block";
+    } else {
+        errorInput[0].style.display = "none";
+        errorInput[1].style.display = "none";
+        errorInput[2].style.display = "none";
+        errorInput[3].style.display = "none";
+        errorInput[5].style.display = "none";
+        errorInput[6].style.display = "none";
+        errorInput[7].style.display = "none";
+        errorInput[8].style.display = "none";
+    }
+}
+
+/*message if user inputs letters instead of numbers*/
+function numbersOnly() {
+    var zip = document.getElementById('txtzip');
+    var Bzip = document.getElementById('txtzip_billing');
+
+    var zips = "^[A-Za-z]+$";
+    if (zip.value.match(zips) && document.activeElement == zip) {
+        errorInput[4].style.display = "block";
+    } else if (Bzip.value.match(zips) && document.activeElement == Bzip) {
+        errorInput[9].style.display = "block";
+    } else {
+        errorInput[4].style.display = "none";
+        errorInput[9].style.display = "none";
+    }
+}
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
---
+++ week15:final/js/contact-form-validation.js
@@ -0,0 +1,57 @@
+/*CHECK VALID INPUT*/
+var errorInput = document.getElementsByClassName('invalid-input');
+
+/*message if user inputs numbers instead of letters*/
+function alphaOnly() {
+    var name = document.getElementById('name');
+
+    var numbers = "^[0-9*#+@]+$";
+    if (name.value.match(numbers) && document.activeElement == name) {
+        errorInput[0].style.display = "block";
+    } else {
+        errorInput[0].style.display = "none";
+    }
+}
+
+/*checks for @ format*/
+function emailOnly() {
+    var email = document.getElementById('email');
+
+    var emails = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+    if (email.value.match(emails)) {
+        errorInput[1].style.display = "none";
+    } else {
+        errorInput[1].style.display = "block";
+    }
+}
+
+/*message if user inputs letters instead of numbers*/
+function numbersOnly() {
+    var phone = document.getElementById('phone');
+    
+    var phones = "^[A-Za-z]+$";
+    if (phone.value.match(phones)) {
+        errorInput[2].style.display = "block";
+    } else {
+        errorInput[2].style.display = "none";
+    }
+}
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
